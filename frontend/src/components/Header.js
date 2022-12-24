1cab4f9af7906e7392250f1e6d5adb88f816b9f6
Cold <samedovvv00@gmail.com>
2022-12-24 21:56:46 +0400

    added logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Navbar, Container } from 'react-bootstrap'
+import { ReactComponent as Logo } from '../images/ImagesGallery.svg'
 
 const Header = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+          <Logo style={{maxWidth: '14rem', maxHeight: '3rem'}} />
         </Container>
       </Navbar>
     </>
